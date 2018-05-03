d556c549bb25812b00b87501936d1b6b3531c764
sammy paiz <sammypaiz@sammypaiz.local>
2018-05-03 14:48:03 -0700

    slide menu animate

--- js/jquery.js
+++ js/jquery.js
@@ -2,5 +2,6 @@ $(document).ready(function() {
     $('#menu').click(function() {
        $('#slideMenu').animate({width:'toggle'}, 3000); 
         $('#slideMenu').attr('style', 'display:block');
+        $('.hidden').attr('style', 'display:block');
     });
 });
