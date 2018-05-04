4441d0aa347a77686730d0012d7c20823bdf344c
sammy paiz <sammypaiz@sammypaiz.local>
2018-05-03 20:49:08 -0700

    toggleClass on a div

--- js/jquery.js
+++ js/jquery.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
-    $('#menu').click(function() {
-       $('#slideMenu').animate({width:'toggle'}, 3000); 
-        $('#slideMenu').attr('style', 'display:block');
-        $('.hidden').attr('style', 'display:block');
-    });
+   $('#menu').click(function() {
+      $('#menu').toggleClass('menu-open'); 
+   });
 });
