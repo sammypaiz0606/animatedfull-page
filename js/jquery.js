349c3aa1fb1427dc49272bfd883bc879f192d258
sammy paiz <sammypaiz@sammypaiz.local>
2018-05-06 13:12:49 -0700

    slideFadeLeft animate menu

--- js/jquery.js
+++ js/jquery.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
-   $('#menu').click(function() {
-      $('#menu').toggleClass('menu-open'); 
+   $('#menu-button').click(function() {
+        $('#menu').toggleClass('menu-collapse');
+       $('#menu').toggleClass('hidden animated fadeInLeft');
+        
    });
 });
+
+
+//
+    //  $('#menu-button').toggleClass('menu-collapse menu-open');
+      // $('#menu-button').toggleClass('menu-collapse');
+       //$('#menu ul li').toggleClass('hidden animated fadeInLeft');
+       
