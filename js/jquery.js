ce4ee70ed0369bc73e5cadc340e54d15dd3bb3e5
sammy paiz <sammypaiz@sammypaiz.local>
2018-05-07 13:02:31 -0700

    finished animate menu

--- js/jquery.js
+++ js/jquery.js
@@ -1,14 +1,10 @@
 $(document).ready(function() {
    $('#menu-button').click(function() {
-        $('#menu').toggleClass('menu-collapse');
-       $('#menu').toggleClass('hidden animated fadeInLeft');
+       $('#menu-button').toggleClass('menu-open');
+       $('#menu').toggleClass('menu-collapse');
+       $('#menu ul li').toggleClass('hidden animated fadeInLeft');
         
    });
 });
 
 
-//
-    //  $('#menu-button').toggleClass('menu-collapse menu-open');
-      // $('#menu-button').toggleClass('menu-collapse');
-       //$('#menu ul li').toggleClass('hidden animated fadeInLeft');
-       
